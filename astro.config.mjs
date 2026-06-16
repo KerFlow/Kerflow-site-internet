1241f4c10e1323328437f3a409446c3453df6cfc
Evadegardin <Evadegardin@users.noreply.github.com>
2026-06-16 19:03:22 +0900

    Add Keystatic configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import keystatic from "@keystatic/astro";
+
+const isBuild = process.env.npm_lifecycle_event === "build";
 
 export default defineConfig({
+  integrations: isBuild ? [] : [react(), keystatic()],
   output: "static"
 });
