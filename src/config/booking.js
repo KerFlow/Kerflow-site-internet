a4e82474c473301d2aca229728362428843026a4
Evadegardin <Evadegardin@users.noreply.github.com>
2026-09-03 13:34:20 +0500

    Integrate HelloResa course planning

---
+++ src/config/booking.js
@@ -0,0 +1,3 @@
+export const HELLORESA_PUBLIC_URL = "https://kerflow.helloresa.com/";
+export const HELLORESA_PLANNING_URL = "https://kerflow.helloresa.com/public/planning";
+export const HELLORESA_EMBED_URL = HELLORESA_PLANNING_URL;
